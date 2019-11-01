c5d9fac0033a6e247961b0ce9cd67e29dd7c1c11
123456 <123456@LAPTOP-SD9KK076>
2019-11-01 11:56:19 +0800

    1

---
+++ sp03-userservice/src/test/java/com/tedu/sp03/Sp03UserserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tedu.sp03;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp03UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sp06-ribbon/src/test/java/com/tedu/sp06/Sp06RibbonApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tedu.sp06;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp06RibbonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
